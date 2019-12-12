49bf2c930f4b1fff2b7e03cabe5077138a2ae9ac
Md Akmal Hossain <akmal2080@gmail.com>
2019-12-12 02:26:52 -0500

    update testbase class

--- POM/src/main/java/com/cloudinfomatrix/qa/base/TestBase.java
+++ POM/src/main/java/com/cloudinfomatrix/qa/base/TestBase.java
@@ -1,5 +1,29 @@
 package com.cloudinfomatrix.qa.base;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+
 public class TestBase {
+	
+	static WebDriver driver;
+	static Properties prop;
+	
+	public TestBase()  {
+		try {
+			prop =new Properties();
+			FileInputStream fis= new FileInputStream("/PageObjectModel/src/main/java/com/cloudinfomatrix/qa/config/config.properties");
+			prop.load(fis);
+		}catch(FileNotFoundException e){
+			e.printStackTrace();
+			
+			
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
