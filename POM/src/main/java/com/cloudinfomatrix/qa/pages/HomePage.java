b74c7a08f38b231f1c6c5f34b208a9402d101953
akmalcode <akmalscode@hotmail.com>
2023-08-30 23:33:47 -0400

    jj

--- POM/src/main/java/com/cloudinfomatrix/qa/pages/HomePage.java
+++ POM/src/main/java/com/cloudinfomatrix/qa/pages/HomePage.java
@@ -1,7 +1,17 @@
 package com.cloudinfomatrix.qa.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 import com.cloudinfomatrix.qa.base.TestBase;
 
 public class HomePage extends TestBase{
+	
+	@FindBy(xpath="//*[@id=\"dashboardRoot\"]/span/div/div/div[1]/div/div[1]/div[2]/div[2]/div/div[2]/ul/li[1]/div[2]/div/div/a/div/div/span/span")
+	WebElement payments;
+	
+	
+	
+	
 
 }
